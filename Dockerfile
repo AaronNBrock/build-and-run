7a5e9c84aee7eefea46b54c99c4405ee60b56820
Aaron N. Brock <Contact@AaronNBrock.com>
2018-07-13 00:07:12 -0400

    Added argument options

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM alpine:3.8
 RUN apk update
 RUN apk add docker
 RUN apk add git
+RUN apk add bash
 
 WORKDIR /workspace
 COPY . .
--- build.sh
+++ build.sh
@@ -1,21 +1,54 @@
-#!/bin/sh
+#!/bin/bash
 
-# git clone $1 ./project
-# cd ./project
-USAGE="Usage: build.sh <repository>"
+set -e # If program fails -> fail script
+
+USAGE="
+Usage: ./build.sh [OPTIONS] REPOSITORY
+
+Options:
+  -h, --help              Display this message.
+  -r, --run-args          Arguments added into command \"docker run <here> image\".  (default: \"\")
+  -c, --container-args    Arguments added into command \"docker run image <here>\".  (default: \"\")
+
+"
+
+
+# FROM: https://stackoverflow.com/a/14203146/7607701
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+    printf "$USAGE"
+    exit 0
+    ;;
+    -r|--run-args)
+    RUN_ARGS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -c|--container-args)
+    CONTAINER_ARGS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
 
 if [ $# -eq 0 ]; then
-	echo "No repository given\n"
-	echo $USAGE
+	printf "\nError: No repository given\n"
+	echo "$USAGE"
 	exit 1
-fi
-
-
-if [ "$1" = "-h" ] [ "$1" = "--help" ]; then
-	echo $USAGE
-	exit 0
 fi
 
 TAG="builder"
 docker build -t $TAG -f build.dockerfile $1
-docker run -v /var/run/docker.sock:/var/run/docker.sock $TAG
+docker run -v /var/run/docker.sock:/var/run/docker.sock $RUN_ARGS $TAG $CONTAINER_ARGS
